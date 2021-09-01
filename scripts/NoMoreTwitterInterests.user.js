de44fc4e515c33c5abc77b6ea927ee25740adf71
Midori <suienzan@gmail.com>
2021-09-01 17:27:01 +0900

    support `mobile.twitter.com`

--- scripts/NoMoreTwitterInterests.user.js
+++ scripts/NoMoreTwitterInterests.user.js
@@ -19,7 +19,7 @@
 const TOPIC_PATH="M18.265 3.314c-3.45-3.45-9.07-3.45-12.52 0-3.45 3.44-3.45 9.06 0 12.51 1.5 1.49 3.43 2.38 5.51 2.56v4.14c0 .31.2.6.5.7.08.03.17.05.25.05.22 0 .44-.1.59-.29l6.49-8.11c2.63-3.49 2.27-8.47-.82-11.56zm-10.56 7.87c0-.41.33-.75.75-.75h4.05c.41 0 .75.34.75.75s-.34.75-.75.75h-4.05c-.42 0-.75-.34-.75-.75zm8.6-3.24c0 .42-.34.75-.75.75h-7.1c-.42 0-.75-.33-.75-.75 0-.41.33-.75.75-.75h7.1c.41 0 .75.34.75.75z";
 
 setInterval(function () {
-	let elements = document.querySelectorAll('svg.r-1janqcz.r-10ptun7');
+	let elements = document.querySelectorAll('svg.r-1janqcz.r-10ptun7, svg.r-14j79pv.r-1b94p3d');
   elements.forEach(function (node) {
 		if (node?.querySelector('path')?.getAttribute("d") === TOPIC_PATH) {
 			var p = node.parentElement;
